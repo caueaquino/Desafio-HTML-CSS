8ceb8c783d5c4ee94d434e2ce3a7b60e9fc4acf2
caueaquino <cauenogueira@alunos.utfpr.edu.br>
2019-02-11 01:37:13 -0200

    update js-style

--- js/js-style.js
+++ js/js-style.js
@@ -1,2 +1,56 @@
 // Javascript Document
 
+function bthome(){
+    document.getElementById("ahome").style.borderRadius="0px 0px 0px 40px";
+    document.getElementById("ahome").style.marginLeft="10%";
+    document.getElementById("ahome").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("ahome").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("lihome").style.zIndex="3";
+}
+
+function btcontato(){
+    document.getElementById("acontatos").style.borderRadius="40px 0px 0px 40px";
+    document.getElementById("acontatos").style.marginLeft="10%";
+    document.getElementById("acontatos").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("acontatos").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("licontatos").style.zIndex="3";
+}
+
+function btfavorito(){
+    document.getElementById("afavoritos").style.borderRadius="40px 0px 0px 40px";
+    document.getElementById("afavoritos").style.marginLeft="10%";
+    document.getElementById("afavoritos").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("afavoritos").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("lifavoritos").style.zIndex="3";
+}
+
+function btlogout(){
+    document.getElementById("asair").style.borderRadius="40px 0px 0px 0px";
+    document.getElementById("asair").style.marginLeft="10%";
+    document.getElementById("asair").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("asair").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("lisair").style.zIndex="3";
+}
+
+function resetbts(){
+    document.getElementById("ahome").style.borderRadius="0px 0px 0px 0px";
+    document.getElementById("ahome").style.marginLeft="0%";
+    document.getElementById("ahome").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("ahome").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("lihome").style.zIndex="3";
+    document.getElementById("acontatos").style.borderRadius="0px 0px 0px 0px";
+    document.getElementById("acontatos").style.marginLeft="0%";
+    document.getElementById("acontatos").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("acontatos").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("licontatos").style.zIndex="3";
+    document.getElementById("afavoritos").style.borderRadius="0px 0px 0px 0px";
+    document.getElementById("afavorito").style.marginLeft="0%";
+    document.getElementById("afavoritos").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("afavoritos").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("lifavoritos").style.zIndex="3";
+    document.getElementById("asair").style.borderRadius="0px 0px 0px 0px";
+    document.getElementById("asair").style.marginLeft="0%";
+    document.getElementById("asair").style.backgroundColor="rgb(10,10,10)";
+    document.getElementById("asair").style.boxShadow="-2px 0px 10px rgb(10, 10, 10);"
+    document.getElementById("lisair").style.zIndex="3";
+}
