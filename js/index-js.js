fc8ce6604dc42dcc107323a3f144050fb19d76d9
Cauê Aquino Nogueira <caue@FORLOGIC.LOCAL>
2019-02-14 15:10:34 -0200

    final day

--- js/index-js.js
+++ js/index-js.js
@@ -23,6 +23,11 @@ let contato={
     id: ""
 }
 
+function setData(){
+    
+    btContato();
+}
+
 function btMenu(){
     if(y==0){
         document.getElementById("container-side").style.display="flex";
@@ -63,7 +68,12 @@ function btConfirmarCadastro(){
     
     contatos.push(contato);
 
+    if(contato.isFavorite==true){
+        favoritos.push(contato);
+    }
+
     limpaTelaCadastro();
+    btContato();
 }
 
 function btCancelarCadastro(){
@@ -85,15 +95,61 @@ function limpaTelaCadastro(){
 }
 
 function contactView(){
+    document.getElementById("eavatar").value="./img/user.png";
+    document.getElementById("enome").value="caue";
+    document.getElementById("esobrenome").value="aquino nogueira";
+    document.getElementById("eemail").value="caue@forlogic.net";
+    document.getElementById("ecompanhia").value="forlogic";
+    document.getElementById("eendereco").value="rua avenida cidade";
+    document.getElementById("etelefone").value="11964984447";
+    document.getElementById("ecomentario").value="algum comentario";
     document.getElementById("container-vcont").style.display="flex";
 }
 
+function editContact(){
+    document.getElementById("eavatar").disabled=false;
+    document.getElementById("enome").disabled=false;
+    document.getElementById("esobrenome").disabled=false;
+    document.getElementById("eemail").disabled=false;
+    document.getElementById("ecompanhia").disabled=false;
+    document.getElementById("eendereco").disabled=false;
+    document.getElementById("etelefone").disabled=false;
+    document.getElementById("ecomentario").disabled=false;
+
+    document.getElementById("btEditSair").style.display="none";
+    document.getElementById("btEditConf").style.display="block";
+    document.getElementById("btEditCanc").style.display="block";
+}
+
 function btConfirmarPerfil(){
-    document.getElementById("container-vcont").style.display="none";
+    disableEdit();
+    hideEdit();
 }
 
 function btCancelarPerfil(){
+    disableEdit();
+    hideEdit();
+}
+
+function btCloseView(){
     document.getElementById("container-vcont").style.display="none";
+}
+
+function disableEdit(){
+    document.getElementById("eavatar").disabled=true;
+    document.getElementById("enome").disabled=true;
+    document.getElementById("esobrenome").disabled=true;
+    document.getElementById("eemail").disabled=true;
+    document.getElementById("ecompanhia").disabled=true;
+    document.getElementById("eendereco").disabled=true;
+    document.getElementById("etelefone").disabled=true;
+    document.getElementById("ecomentario").disabled=true;
+}
+
+function hideEdit(){
+    document.getElementById("btEditConf").style.display="none";
+    document.getElementById("btEditCanc").style.display="none";
+    document.getElementById("btEditSair").style.display="block";
 }
 
 function btContato(){
@@ -101,9 +157,34 @@ function btContato(){
     document.getElementById("acontatos").style.backgroundColor="rgb(35, 68, 255)";
     document.getElementById("acontatos").style.boxShadow="2px 0px 10px rgb(10, 10, 10)";
     document.getElementById("licontatos").style.zIndex="3";
-    x=1;
+    document.getElementById("not-contacts").style.display="none";
+    x=0;
 
     document.getElementById("h2-type").innerText="Contatos";
+    if(contatos.length>0){
+
+        let x=document.getElementById("contacts-field");
+
+        for(i=0;i<10||i<contatos.length;i++){
+
+            let d=document.createElement("div");
+
+            d.innerHTML='<div class="contact-data">'+
+                            '<img src="'+contatos[i].info.avatar+'" height="230px" width="230px">'+
+                            '<label class="lcontact">Contato:</label><label>'+contatos[i].firstName+'&nbsp'+contatos[i].lastName+'</label>'+
+                                
+                            '<div class="internal-view">'+
+                                '<button onclick="contactView()">Visualizar'+
+                                    '<img src="./img/icon-view.png" alt="Icone-Visualizar" height="30px" width="30px">'+
+                                '</button>'+
+                            '</div>'+
+                        '</div>';
+
+            x.appendChild(d);  
+        }
+    }else{
+        document.getElementById("not-contacts").style.display="block";
+    }
 }
 
 function btFavorito(){
@@ -111,9 +192,35 @@ function btFavorito(){
     document.getElementById("afavoritos").style.backgroundColor="rgb(35, 68, 255)";
     document.getElementById("afavoritos").style.boxShadow="2px 0px 10px rgb(10, 10, 10)";
     document.getElementById("lifavoritos").style.zIndex="3";
-    x=2;
+    document.getElementById("not-favorits").style.display="none";
+    x=1;
 
     document.getElementById("h2-type").innerText="Favoritos";
+
+    if(favoritos.length>0){
+
+        let x=document.getElementById("contacts-field");
+
+        for(i=0;i<10||i<favoritos.length;i++){
+            
+            let d=document.createElement("div");
+
+            d.innerHTML='<div class="contact-data">'+
+                            '<img src="'+favoritos[i].info.avatar+'" height="230px" width="230px">'+
+                            '<label class="lcontact">Contato:</label><label>'+favoritos[i].firstName+'&nbsp'+favoritos[i].lastName+'</label>'+
+                            
+                            '<div class="internal-view">'+
+                                '<button id="'+favoritos[i].id+'" onclick="contactView()">Visualizar'+
+                                    '<img src="./img/icon-view.png" alt="Icone-Visualizar" height="30px" width="30px">'+
+                                 '</button>'+
+                            '</div>'+
+                        '</div>';
+
+            x.appendChild(d);
+        }
+    }else{
+        document.getElementById("not-favorits").style.display="block";
+    }
 }
 
 function resetbts(){
@@ -123,4 +230,6 @@ function resetbts(){
     document.getElementById("afavoritos").style.backgroundColor="rgba(10, 10, 10, 0)";
     document.getElementById("afavoritos").style.boxShadow="-2px 0px 10px rgba(10, 10, 10, 0)";
     document.getElementById("lifavoritos").style.zIndex="2";
+    document.getElementById("not-contacts").style.display="none";
+    document.getElementById("not-favorits").style.display="none";
 }
